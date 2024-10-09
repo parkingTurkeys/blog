9bbe102559b28dc3578db4d499894b79cadc9530
parking_turkeys <roya1060wien@gmail.com>
2024-10-09 02:07:05 +0200

    renamed files, added a separate data file, made better titles.

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 
-const articleNames = [
-    "My blog is ready!", "Hey there!"
-]
 
-const articleSubtitles = [
-    "...finally", "its @parking_turkeys"
-]
-
-const articles = [
-    "So, my blog is finally ready! I guess you know that though, since you're here. My blog is just all around cool and will have loads of fun stuff, and maybe some stories.", "hello"
-]
-
-const titles = [
-    "new blog!", "hey"
-]
 getArticles()
 
 
