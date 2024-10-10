c0a22edc25f35d4318e4023b83f97075b9a3e0c1
parking_turkeys <roya1060wien@gmail.com>
2024-10-10 22:26:51 +0200

    more data

---
+++ data/storydata.js
@@ -0,0 +1,15 @@
+const articleNames = [
+    "My amazing story is ready!"
+]
+
+const articleSubtitles = [
+    "...well the website at least"
+]
+
+const articles = [
+    "So, my story is finally ready! I guess you know that though, since you're here. My story is just all around cool and will have loads of fun stuff, and maybe some stories."
+]
+
+const titles = [
+    "new story"
+]
