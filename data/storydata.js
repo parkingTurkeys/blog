e4df392a8728a677bea31aa6961a2cde11be39e3
parking_turkeys <roya1060wien@gmail.com>
2024-10-11 22:27:55 +0200

    move ~index~blog.html to html/...

--- data/storydata.js
+++ data/storydata.js
@@ -7,7 +7,7 @@ const articleSubtitles = [
 ]
 
 const articles = [
-    "Hey, welcome to my story page! The page is different because this is licensed under CC-BY, and you have to attribute me if you want to use it! The story is <del>not started</del> still eing worked on"
+    "Hey, welcome to my story page! The page is different because this is licensed under CC-BY, and you have to attribute me if you want to use it! The story is <del>not started</del> still being worked on, but it's going to be set in an airport and be a cringy gay romance!"
 ]
 const titles = [
     "new story"
