669fc3da326885325af25c6a3dd2017a804c8f98
parking_turkeys <roya1060wien@gmail.com>
2024-10-20 21:51:12 +0200

    add webring and articles page

---
+++ data/links.js
@@ -0,0 +1,15 @@
+const articleNames = [
+    "This page was designed to last", 
+]
+
+const articleSubtitles = [
+    "article by jeff huang", 
+]
+
+const articles = [
+    "<a href='https://jeffhuang.com/designed_to_last/'>https://jeffhuang.com/designed_to_last/</a>",
+]
+
+const titles = [
+    "This page was designed to last",
+]
